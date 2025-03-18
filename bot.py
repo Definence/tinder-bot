00750a0d575953ccc4d9988bd5aadf7aebbac461
Definence <dp210592ybo@gmail.com>
2025-03-18 22:24:56 +0200

    Implement test date functionality

--- bot.py
+++ bot.py
@@ -25,35 +25,54 @@ async def gpt(update, context):
   await send_photo(update, context, 'gpt')
   await send_text(update, context, load_message('gpt'))
 
+async def send_text_handler(update, context):
+  if dialog.mode == 'gpt':
+    await gpt_dialog(update, context)
+  elif dialog.mode == 'date':
+    await date_dialog(update, context)
+  else:
+    await send_text(update, context, 'Hello ' + update.message.text)
+
 async def gpt_dialog(update, context):
   text = update.message.text
   prompt = load_prompt('gpt')
   answer = await chatgpt.send_question(prompt, text)
   await send_text(update, context, answer)
 
-async def send_text_handler(update, context):
-  if dialog.mode == 'gpt':
-    await gpt_dialog(update, context)
-  else:
-    await send_text(update, context, 'Hello' + update.message.text)
+async def date(update, context):
+  dialog.mode = 'date'
+  await send_photo(update, context, 'date')
+  await send_text_buttons(update, context, load_message('date'), {
+    'date_grande': 'Аріана Гранде',
+    'date_robbie': 'Марго Роббі',
+    'date_zendaya': 'Зендея',
+    'date_gosling': 'Райан Гослінг',
+    'date_hardy': 'Том Харді',
+  })
+
+async def date_button(update, context):
+  dialog.mode = 'date' # just in case triggered in other mode
+  query = update.callback_query.data
+  await update.callback_query.answer()
+  await send_photo(update, context, query)
+  await send_text(update, context, 'Гарний вибір.\uD83D\uDE05 Ваша задача запросити дівчину / хлопця за пʼять повідомлень\uFE0F')
+  chatgpt.set_prompt(load_prompt(query))
+
+async def date_dialog(update, context):
+  # pdb.set_trace()
+  text = update.message.text
+  typing_msg  = await send_text(update, context, 'Набирає повідомлення...')
+  answer = await chatgpt.add_message(text)
+  await typing_msg.edit_text(answer)
+
 
 dialog = Dialog()
 dialog.mode = None
-
 chatgpt = ChatGptService(token=OPEN_AI_TOKEN)
-
 app = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
 app.add_handler(CommandHandler('start', start))
 app.add_handler(CommandHandler('gpt', gpt))
+app.add_handler(CommandHandler('date', date))
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, send_text_handler))
-
-# async def buttons_handler(update, context):
-#   query = update.callback_query.data
-#   # pdb.set_trace()
-#   if query == 'start':
-#     await send_text(update, context, 'Started')
-#   elif query == 'stop':
-#     await send_text(update, context, 'Stopped')
-# app.add_handler(CallbackQueryHandler(buttons_handler))
-
+app.add_handler(CallbackQueryHandler(date_button, pattern='^date_.*'))
 app.run_polling()
